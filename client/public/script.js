824ec462c76f0821369cf942cfabc152a5f120ff
Edward Casbon <edward@edwardcasbon.co.uk>
2017-11-07 17:00:37 +0000

    Client updates

--- client/public/script.js
+++ client/public/script.js
@@ -1,5 +1,5 @@
 const socket = new WebSocket('ws://localhost:3000');
-const player = document.querySelector('audio');
+const player = document.querySelector('video');
 const playlist = document.querySelectorAll('.playlist a');
 const streamId = window.location.href.split('/').reverse().shift();
 
@@ -31,22 +31,66 @@ socket.addEventListener('message', event => {
     switch (message.type) {
         case 'STREAM_URL':
             console.log(`Update stream to '${message.data.url}'`);
+            player.src = message.data.url;
             break;
 
         case 'STREAM_PLAY':
             console.log('Play stream');
+            player.play();
             break;
 
         case 'STREAM_PAUSE':
             console.log('Pause stream');
+            player.pause();
             break;
 
         case 'STREAM_STOP':
             console.log('Stop stream');
+            player.pause();
             break;
 
+        case 'STREAM_POSITION':
+            console.log('Update position');
+            player.currentTime = message.data.position;
+
         case 'STREAM_REQUEST_POSITION':
-            console.log('Send current stream position (seek)')
+            console.log('Send current stream position (seek)');
+            socket.send(JSON.stringify({
+                type: 'STREAM_POSITION',
+                data: {
+                    streamId,
+                    position: player.currentTime
+                }
+            }));
             break;
     }
+});
+
+player.addEventListener('play', e => {
+    socket.send(JSON.stringify({
+        type: 'STREAM_PLAY',
+        data: {
+            streamId
+        }
+    }));
+});
+
+player.addEventListener('pause', e => {
+    socket.send(JSON.stringify({
+        type: 'STREAM_PAUSE',
+        data: {
+            streamId
+        }
+    }));
+});
+
+player.addEventListener('seeked', e => {
+    console.log('Seeked to ' + player.currentTime);
+//     socket.send(JSON.stringify({
+//         type: 'STREAM_POSITION',
+//         data: {
+//             streamId,
+//             position: player.currentTime
+//         }
+//     }));
 });
